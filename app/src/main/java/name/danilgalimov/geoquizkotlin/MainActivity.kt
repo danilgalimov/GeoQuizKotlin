197cba75a61b047284894bcfc3e42ddf7900e4ad
Danil Galimov <danilgalimov26@gmail.com>
2022-04-25 21:12:41 +0500

    Добавление анимации перехода на версии выше или равной M.

--- app/src/main/java/name/danilgalimov/geoquizkotlin/MainActivity.kt
+++ app/src/main/java/name/danilgalimov/geoquizkotlin/MainActivity.kt
@@ -1,8 +1,10 @@
 package name.danilgalimov.geoquizkotlin
 
 import android.app.Activity
+import android.app.ActivityOptions
 import android.app.Instrumentation
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -55,10 +57,16 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
-        cheatButton.setOnClickListener {
+        cheatButton.setOnClickListener { view ->
             val answerIsTrue = quizViewModel.currentQuestionAnswer
             val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
-            startActivityForResult(intent, REQUEST_CODE_CHEAT)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val options =
+                    ActivityOptions.makeClipRevealAnimation(view, 0, 0, view.width, view.height)
+                startActivityForResult(intent, REQUEST_CODE_CHEAT, options.toBundle())
+            } else {
+                startActivityForResult(intent, REQUEST_CODE_CHEAT)
+            }
         }
 
         updateQuestion()
